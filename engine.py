8c5f5c4c81840ce1bec41ddd328e3e28ba39df37
nip403 <41174986+nip403@users.noreply.github.com>
2018-08-03 21:41:46 +0800

    Add files via upload

---
+++ engine.py
@@ -0,0 +1,53 @@
+import pygame
+import random
+import sys
+
+class Star:
+    def __init__(self,x,y,z,surf,speed):
+        self.ranges = [x,y,z]
+
+        self.fov = 128
+        
+        self.pos = self.get_pos(x,y,z)
+        self.surf = surf
+        self.speed = speed
+        self.centre = [i/2 for i in [self.surf.get_width(),self.surf.get_height()]]
+
+    def get_pos(self,x,y,z):
+        return {"x":random.randrange(-x/2,x/2),"y":random.randrange(-y/2,y/2),"z":random.randrange(1,z)}
+
+    def draw(self):
+        try:
+            pygame.draw.circle(self.surf,[(1-self.pos["z"]/self.ranges[2])*255]*3,list(map(int,(self.pos["x"]*(self.fov/self.pos["z"])+self.centre[0],self.pos["y"]*(self.fov/self.pos["z"])+self.centre[1]))),int((1-self.pos["z"]/self.ranges[2])*2.5),0)
+        except:
+            pass
+        
+    def animate(self):
+        self.pos["z"] -= self.speed
+        self.pos = self.get_pos(self.ranges[0],self.ranges[1],self.ranges[2]) if self.pos["z"] <= 0 or not all(0 <= i <= [self.surf.get_width(),self.surf.get_height()][p] for p,i in enumerate((self.pos["x"]*(self.fov/self.pos["z"])+self.centre[0],self.pos["y"]*(self.fov/self.pos["z"])+self.centre[1]))) else self.pos
+        
+class Starfield:
+    def __init__(self,amount,surf,xrange=100,yrange=100,zrange=100,speedrange=1):
+        self.stars = [Star(xrange,yrange,zrange,surf,random.randrange(4-speedrange,4+speedrange)/5) for _ in range(amount)]
+
+    def draw(self):
+        for star in self.stars:
+            star.draw()
+
+    def animate(self):
+        for star in self.stars:
+            star.animate()
+
+    def events(self,**custom):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+
+            else:
+                for e in custom.keys():
+                    if e == event.type:
+                        try:
+                            custom[e]()
+                        except Exception as err:
+                            print("Error: %s" % err)
---
+++ main.py
@@ -0,0 +1,49 @@
+from engine import Starfield
+import pygame
+
+# SETTINGS
+refresh_screen = True
+big_bang_effect = False
+follow_mouse = False
+
+s = [800,600]
+
+pygame.init()
+screen = pygame.display.set_mode(s,0,32)
+clock = pygame.time.Clock()
+pygame.display.set_caption("3D Starfield by NIP")
+
+def bbe(mode,s):
+    for i in getattr(s,"stars"):
+        i.fov += 2 if mode else -2
+    
+    return mode if -500 <= i.fov <= 500 else not mode
+
+def follow(s):
+    for i in getattr(s,"stars"):
+        i.centre = pygame.mouse.get_pos()
+
+def main():
+    sf = Starfield(2000,screen)
+    mode = True
+
+    while True:
+        clock.tick(60)
+
+        if refresh_screen:
+            screen.fill((0,0,0))
+
+        if big_bang_effect:
+            mode = bbe(mode,sf)
+
+        if follow_mouse:
+            follow(sf)
+        
+        sf.events()
+        sf.draw()
+        sf.animate()
+
+        pygame.display.flip()
+
+if __name__ == "__main__":
+    main()
