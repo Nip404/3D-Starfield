43b6bdcbd3ce427fc33c8f95c976d9c983e98caf
Nip404 <57417504+Nip404@users.noreply.github.com>
2019-11-06 00:08:22 +0000

    Update and rename main.py to starfield.py

--- starfield.py
+++ starfield.py
@@ -3,38 +3,38 @@ import pygame
 
 # SETTINGS
 refresh_screen = True
-effect = False
+effect1 = False
 follow_mouse = False
 
-s = [800,600]
+s = [800, 600]
 
 pygame.init()
-screen = pygame.display.set_mode(s,0,32)
+screen = pygame.display.set_mode(s, 0, 32)
 clock = pygame.time.Clock()
 pygame.display.set_caption("3D Starfield by NIP")
 
-def bbe(mode,s):
-    for i in getattr(s,"stars"):
+def eff1(mode, s):
+    for i in getattr(s, "stars"):
         i.fov += 2 if mode else -2
     
     return mode if -500 <= i.fov <= 500 else not mode
 
 def follow(s):
-    for i in getattr(s,"stars"):
+    for i in getattr(s, "stars"):
         i.centre = pygame.mouse.get_pos()
 
 def main():
-    sf = Starfield(2000,screen)
+    sf = Starfield(2000, screen)
     mode = True
 
     while True:
         clock.tick(60)
 
         if refresh_screen:
-            screen.fill((0,0,0))
+            screen.fill((0, 0, 0))
 
-        if bb_effect:
-            mode = bbe(mode,sf)
+        if effect1:
+            mode = eff1(mode, sf)
 
         if follow_mouse:
             follow(sf)
